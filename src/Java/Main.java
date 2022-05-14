85e4ac0e06fd4d85968161305c6eedf3a0db67db
coddinglearn <89706606+coddinglearn@users.noreply.github.com>
2022-05-14 22:51:52 +0200

    Initial commit

---
+++ src/Java/Main.java
@@ -0,0 +1,17 @@
+package Java;
+import java.awt.*;
+public class Main {
+
+    public static void main(String[] args) throws AWTException {
+	VirusV1 v1=new VirusV1();
+    for (int i = 0; i <100; i++) {
+        v1.BlockAll();
+        v1.popup();
+  }
+}
+}
+
+
+
+
+
