28a22cd414d889dfe50d4e1b9481b064132ee6d4
coddinglearn <89706606+coddinglearn@users.noreply.github.com>
2022-05-15 18:46:03 +0200

    add file RandomText

--- src/main/java/com/joo/jframevirus/RandomFrame.java
+++ src/main/java/com/joo/jframevirus/RandomFrame.java
@@ -2,16 +2,21 @@ package com.joo.jframevirus;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class RandomFrame extends JFrame {
-    private static final String[] randomText = {
-            "I'm not a virus",
-            "god take you",
-            "suffering hurts you",
-            "eat straw",
-            "O bull"
-    };
+    private String line;
+//    private static final String[] randomText = {
+//            "I'm not a virus",
+//            "god take you",
+//            "suffering hurts you",
+//            "eat straw",
+//            "O bull"
+//    };
 
     public RandomFrame(Dimension screenSize) {
         super("HHHHHHH HHHHHHHH");
@@ -34,10 +39,28 @@ public class RandomFrame extends JFrame {
         this.add(label);
     }
 
+//    private String getRandomText() {
+//        return randomText[(int) (Math.random() * randomText.length)];
+//    }
+    ArrayList<String> words = new ArrayList<>();
+    // add words to array list
     private String getRandomText() {
-        return randomText[(int) (Math.random() * randomText.length)];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new
+                    FileReader("Files/randomText"));
+            line = reader.readLine();
+            while (line != null) {
+                words.add( line );
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words.get((int) (Math.random() * words.size()));
     }
-
     private Point getRandomLocation(Dimension screenSize) {
         return new Point(
                 (int) (Math.random() * screenSize.width),
